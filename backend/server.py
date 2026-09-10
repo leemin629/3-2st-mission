a40f6f271e91d9e4a0a9bb71450b188abdf629fb
h <example@gmil.com>
2026-09-10 21:00:34 +0900

    feat: NVDA 실시간 주가 조회 기능 구현

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI    # ← 추가! FastAPI 도구 가져오기
+from fastapi import FastAPI   
+from fastapi.middleware.cors import CORSMiddleware # ← 추가! FastAPI 도구 가져오기
 import requests
 import os
 from dotenv import load_dotenv
 
 app = FastAPI()                # ← 추가! app을 실제로 만들기 🏠
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # 모든 곳에서의 요청 허용
+    allow_methods=["*"],      # 모든 방식 허용
+    allow_headers=["*"],
+)
 
 load_dotenv()
 API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
